142d6d35af8640877172ad4f0859071d7cc7806f
Manu Phatak <manu.phatak@procore.com>
2020-06-20 11:38:06 -0700

    chore(*): update package details for the build scripts

--- packages/scripts/__tests__/federation-demo-scripts.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const federationDemoScripts = require('..');
-
-describe('@bionikspoon/federation-demo-scripts', () => {
-    it('needs tests');
-});
--- packages/scripts/lib/federation-demo-scripts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = federationDemoScripts;
-
-function federationDemoScripts() {
-    // TODO
-}
